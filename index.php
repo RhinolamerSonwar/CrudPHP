4ac4d79ad227cde1debc81b4047f4ec66b620697
RhinolamerSonwar <rhinolamer@gmail.com>
2022-06-30 14:27:51 +0300

    includes folder and done

--- index.php
+++ index.php
@@ -1,51 +1,41 @@
 <?php session_start();
 require 'dbcon.php'; ?>
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-</head>
-
-<body>
-    <div class="container mt-4">
-        <?php include('message.php'); ?>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h4>Student Details
-                            <a href="student-create.php" class="btn btn-primary float-end">Add Student</a>
-                        </h4>
-                    </div>
-                    <div class="card-body">
-                        <table class="table table-bordered table-striped">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                    <th>Phone</th>
-                                    <th>Course</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                $query = "SELECT * FROM Students";
-                                $result = mysqli_query($conn, $query);
-                                if (mysqli_num_rows($result) > 0) {
-                                    while ($sudent = mysqli_fetch_assoc($result)) {
-                                        echo '<tr>';
-                                        echo '<td>' . $sudent['id'] . '</td>';
-                                        echo '<td>' . $sudent['name'] . '</td>';
-                                        echo '<td>' . $sudent['email'] . '</td>';
-                                        echo '<td>' . $sudent['phone'] . '</td>';
-                                        echo '<td>' . $sudent['course'] . '</td>';
-                                        echo '<td>
+<?php include('./includes/header.php'); ?>
+<div class="container mt-4">
+    <?php include('message.php'); ?>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Student Details
+                        <a href="student-create.php" class="btn btn-primary float-end">Add Student</a>
+                    </h4>
+                </div>
+                <div class="card-body">
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Phone</th>
+                                <th>Course</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $query = "SELECT * FROM Students";
+                            $result = mysqli_query($conn, $query);
+                            if (mysqli_num_rows($result) > 0) {
+                                while ($sudent = mysqli_fetch_assoc($result)) {
+                                    echo '<tr>';
+                                    echo '<td>' . $sudent['id'] . '</td>';
+                                    echo '<td>' . $sudent['name'] . '</td>';
+                                    echo '<td>' . $sudent['email'] . '</td>';
+                                    echo '<td>' . $sudent['phone'] . '</td>';
+                                    echo '<td>' . $sudent['course'] . '</td>';
+                                    echo '<td>
                                             <a href="student-view.php?id=' . $sudent['id'] . '" class="btn btn-info btn-sm">View</a>
                                             <a href="student-edit.php?id=' . $sudent['id'] . '" class="btn btn-primary btn-sm">Edit</a>
                                             <form action = "code.php" method = "POST" class="d-inline">                                                
@@ -54,24 +44,21 @@ require 'dbcon.php'; ?>
                                     
                                             
                                         </td>';
-                                        echo '</tr>';
-                                    }
-                                } else {
-                                    echo '<tr>';
-                                    echo '<td colspan="6">No data found</td>';
                                     echo '</tr>';
                                 }
+                            } else {
+                                echo '<tr>';
+                                echo '<td colspan="6">No data found</td>';
+                                echo '</tr>';
+                            }
 
-                                ?>
-                            </tbody>
-                        </table>
+                            ?>
+                        </tbody>
+                    </table>
 
-                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-</body>
-
-</html>
+</div>
+<?php include('./includes/footer.php'); ?>
--- student-view.php
+++ student-view.php
@@ -2,83 +2,71 @@
 
 require("dbcon.php")
 ?>
-<!doctype html>
-<html lang="en">
+<?php include('./includes/header.php'); ?>
+<div class="container mt-5  ">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hello World</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-</head>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Student View Details
+                        <a href="index.php" class="btn btn-danger float-end">Back</a>
+                    </h4>
+                </div>
+                <div class="card-body">
+                    <?php
+                    if (isset($_GET['id'])) {
+                        $student_id = mysqli_real_escape_string($conn, $_GET['id']);
+                        $sql = "SELECT * FROM Students WHERE id = '$student_id' ";
+                        $result = mysqli_query($conn, $sql);
+                        if (mysqli_num_rows($result) > 0) {
+                            $student = mysqli_fetch_array($result);
+                            $name = $student['name'];
+                            $email = $student['email'];
+                            $phone = $student['phone'];
+                            $course = $student['course'];
+                    ?>
 
-<body>
-    <div class="container mt-5  ">
+                            <form action="code.php" method="POST">
 
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h4>Student View Details
-                            <a href="index.php" class="btn btn-danger float-end">Back</a>
-                        </h4>
-                    </div>
-                    <div class="card-body">
-                        <?php
-                        if (isset($_GET['id'])) {
-                            $student_id = mysqli_real_escape_string($conn, $_GET['id']);
-                            $sql = "SELECT * FROM Students WHERE id = '$student_id' ";
-                            $result = mysqli_query($conn, $sql);
-                            if (mysqli_num_rows($result) > 0) {
-                                $student = mysqli_fetch_array($result);
-                                $name = $student['name'];
-                                $email = $student['email'];
-                                $phone = $student['phone'];
-                                $course = $student['course'];
-                        ?>
-
-                                <form action="code.php" method="POST">
-
-                                    <div class="mb-3">
-                                        <label>Student Name</label>
-                                        <p class="form-control ">
-                                            <?= $name; ?>
-                                        </p>
-                                    </div>
-                                    <div class="mb-3">
-                                        <label>Student Email</label>
-                                        <p class="form-control ">
-                                            <?= $email; ?>
-                                        </p>
-                                    </div>
-                                    <div class="mb-3">
-                                        <label>Student Phone</label>
-                                        <p class="form-control ">
-                                            <?= $phone; ?>
-                                        </p>
-                                    </div>
-                                    <div class="mb-3">
-                                        <label>Student Course</label>
-                                        <p class="form-control ">
-                                            <?= $course; ?>
-                                        </p>
-                                    </div>
+                                <div class="mb-3">
+                                    <label>Student Name</label>
+                                    <p class="form-control ">
+                                        <?= $name; ?>
+                                    </p>
+                                </div>
+                                <div class="mb-3">
+                                    <label>Student Email</label>
+                                    <p class="form-control ">
+                                        <?= $email; ?>
+                                    </p>
+                                </div>
+                                <div class="mb-3">
+                                    <label>Student Phone</label>
+                                    <p class="form-control ">
+                                        <?= $phone; ?>
+                                    </p>
+                                </div>
+                                <div class="mb-3">
+                                    <label>Student Course</label>
+                                    <p class="form-control ">
+                                        <?= $course; ?>
+                                    </p>
+                                </div>
 
 
-                                </form>
-                        <?php
-                            } else {
-                                echo "<h4>Student Not Found</h4>";
-                            }
+                            </form>
+                    <?php
+                        } else {
+                            echo "<h4>Student Not Found</h4>";
                         }
-                        ?>
+                    }
+                    ?>
 
-                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-</body>
+</div>
 
-</html>
+<?php include('./includes/footer.php'); ?>
